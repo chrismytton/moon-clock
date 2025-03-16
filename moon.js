5db87e1e40820d972d82e1a882dc2c5ba782670d
Chris Mytton <mail@chrismytton.com>
2025-03-16 19:50:15 +0000

    Remove SunCalc global

--- moon.js
+++ moon.js
@@ -2,8 +2,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import SunCalc from './vendor/suncalc.js';
 
-window.SunCalc = SunCalc;
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
